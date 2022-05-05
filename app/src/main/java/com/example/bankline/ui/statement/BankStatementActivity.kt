915078016252f4a82ac165d50ab937daa6471d07
maria-serafim-dev <maria.serafim.dev@gmail.com>
2022-05-05 15:26:45 -0300

    Configurando viewbinding e a classe Correntista como parcelable. Enviando o id digitado na WelcomeActivity.kt para a BankStatementActivity.kt

--- app/src/main/java/com/example/bankline/ui/statement/BankStatementActivity.kt
+++ app/src/main/java/com/example/bankline/ui/statement/BankStatementActivity.kt
@@ -2,11 +2,29 @@ package com.example.bankline.ui.statement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.bankline.R
+import com.example.bankline.databinding.ActivityBankStatementBinding
+import com.example.bankline.domain.Correntista
+import java.lang.IllegalArgumentException
 
 class BankStatementActivity : AppCompatActivity() {
+
+    companion object{
+        const  val EXTRA_ACCOUNT_HOLDER = "com.example.bankline.ui.statement.EXTRA_ACCOUNT_HOLDER"
+    }
+
+    private lateinit var binding: ActivityBankStatementBinding
+
+    private val accountHolder by lazy{
+        intent.getParcelableExtra<Correntista>(EXTRA_ACCOUNT_HOLDER) ?: throw IllegalArgumentException()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bank_statement)
+        binding = ActivityBankStatementBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Log.i("ID", accountHolder.id.toString())
     }
 }
--- app/src/main/java/com/example/bankline/ui/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/example/bankline/ui/welcome/WelcomeActivity.kt
@@ -1,12 +1,30 @@
 package com.example.bankline.ui.welcome
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.example.bankline.R
+import androidx.appcompat.app.AppCompatActivity
+import com.example.bankline.databinding.ActivityWelcomeBinding
+import com.example.bankline.domain.Correntista
+import com.example.bankline.ui.statement.BankStatementActivity
 
 class WelcomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWelcomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btContinue.setOnClickListener {
+            val accountHolderId = binding.etAccountHolderId.text.toString().toInt()
+            val accountHolder = Correntista(accountHolderId)
+
+            val intent = Intent(this, BankStatementActivity::class.java).apply{
+                 putExtra(BankStatementActivity.EXTRA_ACCOUNT_HOLDER, accountHolder)
+            }
+            startActivity(intent)
+        }
+
     }
 }
